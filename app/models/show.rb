b4fec913af29a133da5b48ac6280c53036f4f0b2
bswan0002 <bswan0002@gmail.com>
2020-12-01 12:32:56 -0800

    tests pass

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    #should return array of full names of each actor associated with this instance of show
+    self.characters.map {|char| char.actor.full_name}
+  end
 end
